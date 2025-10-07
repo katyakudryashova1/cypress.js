071e5e4ddb93e968dcfa71d5d33936372e400e2b
Katya <kokorinae96@gmail.com>
2025-10-07 22:49:35 +0300

    Add files via upload

---
+++ new_cypress-main/cypress/e2e/login.cy.js
@@ -0,0 +1,91 @@
+describe('Проверка авторизации', function () {
+
+   it('Верный пароль и верный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('qa_one_love1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+   })
+})
+
+describe('Проверка восстановления пароля', function () {
+
+   it('Верный пароль и верный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').contains('Успешно отправили пароль на e-mail');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+   })
+})
+
+
+describe('Проверка авторизации', function () {
+
+   it('Неверный пароль и верный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('qa_123one_love1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+   })
+})
+
+describe('Проверка авторизации', function () {
+
+   it('Верный пароль и неверный логин', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('germa123n@dolnikov.ru');
+        cy.get('#pass').type('qa_one_love1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Такого логина или пароля нет');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+   })
+})
+
+describe('Проверка авторизации', function () {
+
+   it('Верный пароль и логин без @', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('qa_one_love1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Нужно исправить проблему валидации');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+   })
+})
+
+
+describe('Проверка авторизации', function () {
+
+   it('Верный пароль и строчные буквы в логине', function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').should('have.css', 'color', 'rgb(0, 85, 152)');
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('qa_one_love1');
+        cy.get('#loginButton').click();
+        cy.get('#messageHeader').contains('Авторизация прошла успешно');
+        cy.get('#messageHeader').should('be.visible');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+
+   })
+})
